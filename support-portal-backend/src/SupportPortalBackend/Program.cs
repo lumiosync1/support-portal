b7b198b37ade95050989fad0a02bcb2cced767ef
Kurt Nguyen <kurt.nguyen@outlook.com>
2025-07-11 20:36:57 +0700

    Enable legacy timestamp behavior for Npgsql (fix Odata queries on DateTime)

--- support-portal-backend/src/SupportPortalBackend/Program.cs
+++ support-portal-backend/src/SupportPortalBackend/Program.cs
@@ -20,6 +20,9 @@ namespace SupportPortalBackend
     {
         public static void Main(string[] args)
         {
+            // Enable legacy timestamp behavior for Npgsql, this will allow to use DateTime for timestamptz
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
