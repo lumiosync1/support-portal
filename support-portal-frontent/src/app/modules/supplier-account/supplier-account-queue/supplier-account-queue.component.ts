bafe71c26f645b60a95cbd4d4b05a0590ff485ff
Kurt Nguyen <kurt.nguyen@outlook.com>
2025-07-15 16:42:59 +0700

    Supplier Account Queue: calculate max orders in 2h, 4h, 12h

--- support-portal-frontent/src/app/modules/supplier-account/supplier-account-queue/supplier-account-queue.component.ts
+++ support-portal-frontent/src/app/modules/supplier-account/supplier-account-queue/supplier-account-queue.component.ts
@@ -26,6 +26,7 @@ export class SupplierAccountQueueComponent {
 
   data: Object[] = [];
   activeAccounts: number;
+  maxOrders24h: number;
   selectedAccount: any;
 
   ngOnInit(): void {
@@ -60,47 +61,51 @@ export class SupplierAccountQueueComponent {
           account.orders[j] = new Date(order);
         }
 
-        account.orders_24h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 24 * 60 * 60 * 1000));
         account.orders_1h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 1 * 60 * 60 * 1000));
+        account.orders_2h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 2 * 60 * 60 * 1000));
+        account.orders_4h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 4 * 60 * 60 * 1000));
+        account.orders_12h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 12 * 60 * 60 * 1000));
+        account.orders_24h = account.orders.filter((o: Date) => o >= new Date(Date.now() - 24 * 60 * 60 * 1000));
 
-        account.limit1h = account.orders_1h.length >= account.protection_settings.MaxOrders1Hour;
-        account.limit24h = account.orders_24h.length >= account.protection_settings.MaxOrders24Hour;
+        account.limit1h = account.protection_settings.MaxOrders1Hour && account.orders_1h.length >= account.protection_settings.MaxOrders1Hour;
+        account.limit2h = account.protection_settings.MaxOrders2Hour && account.orders_2h.length >= account.protection_settings.MaxOrders2Hour;
+        account.limit4h = account.protection_settings.MaxOrders4Hour && account.orders_4h.length >= account.protection_settings.MaxOrders4Hour;
+        account.limit12h = account.protection_settings.MaxOrders12Hour && account.orders_12h.length >= account.protection_settings.MaxOrders12Hour;
+        account.limit24h = account.protection_settings.MaxOrders24Hour && account.orders_24h.length >= account.protection_settings.MaxOrders24Hour;
 
         // available at
-        if(account.limit1h){
-          account.available_at = new Date(account.last_purchase_time.getTime() + 60 * 60 * 1000);
+        let availableAt = new Date();
+        if(account.limit1h && new Date(new Date(account.orders_1h[0]).getTime() + 60 * 60 * 1000) > availableAt){
+          availableAt = new Date(new Date(account.orders_1h[0]).getTime() + 60 * 60 * 1000);
         }
-        if(account.limit24h){
-          let oldest = account.orders_24h[0] as Date;
-          const avai = new Date(oldest.getTime() + 24 * 60 * 60 * 1000)
-          
-          if(account.available_at) {
-            if(account.available_at < avai) {
-              account.available_at = avai;
-            }
-          } else {
-            account.available_at = avai;
-          }
+        if(account.limit2h && new Date(new Date(account.orders_2h[0]).getTime() + 2 * 60 * 60 * 1000) > availableAt){
+          availableAt = new Date(new Date(account.orders_2h[0]).getTime() + 2 * 60 * 60 * 1000);
+        }
+        if(account.limit4h && new Date(new Date(account.orders_4h[0]).getTime() + 4 * 60 * 60 * 1000) > availableAt){
+          availableAt = new Date(new Date(account.orders_4h[0]).getTime() + 4 * 60 * 60 * 1000);
+        }
+        if(account.limit12h && new Date(new Date(account.orders_12h[0]).getTime() + 12 * 60 * 60 * 1000) > availableAt){
+          availableAt = new Date(new Date(account.orders_12h[0]).getTime() + 12 * 60 * 60 * 1000);
+        }
+        if(account.limit24h && new Date(new Date(account.orders_24h[0]).getTime() + 24 * 60 * 60 * 1000) > availableAt){
+          availableAt = new Date(new Date(account.orders_24h[0]).getTime() + 24 * 60 * 60 * 1000);
         }
         if(account.on_hold_to) {
           const onHoldTo = new Date(account.on_hold_to);
-          if(account.available_at) {
-            if(account.available_at < onHoldTo) {
-              account.available_at = onHoldTo;
-            }
-          } else {
-            account.available_at = onHoldTo;
+          if(availableAt < onHoldTo) {
+            availableAt = onHoldTo;
           }
         }
+        account.available_at = availableAt;
+
         if(!account.allow_purchase) {
           account.available_at = new Date(Date.now() + 48 * 60 * 60 * 1000);
         }
-        if(!account.available_at) {
-          account.available_at = new Date();
-        }
       }
       this.data = accounts;
-      this.activeAccounts = accounts.filter((account: any) => account.allow_purchase).length;
+      const activeAccountList = accounts.filter((account: any) => account.allow_purchase);
+      this.activeAccounts = activeAccountList.length;
+      this.maxOrders24h = activeAccountList.reduce((acc, account) => acc + account.protection_settings.MaxOrders24Hour, 0);
       this.ref.detectChanges();
     });
   }
